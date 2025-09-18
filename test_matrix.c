edb4955ba8fec91b0762947aa781964b56f1a228
Max Holemans <mholemans@clipper.ens.fr>
2025-09-18 15:55:57 +0200

    correction du test

--- test_matrix.c
+++ test_matrix.c
@@ -58,8 +58,12 @@ UTEST(matrix_get, wrong) {
 
 UTEST(matrix_scalar, simple) {
   matrix m = matrix_create(1, 1, 10.);
-  matrix n = matrix_scalar(5., matrix_create(1, 1, 2.));
+  matrix nprime = matrix_create(1, 1, 2.);
+  matrix n = matrix_scalar(5., nprime);
   ASSERT_EQ(*matrix_get(m,0,0), *matrix_get(n,0,0));
+  matrix_destroy(m);
+  matrix_destroy(nprime);
+  matrix_destroy(n);
 }
 
 UTEST_MAIN()  
